4d31bcddae23eaafb8a24381af587eced999a241
Bob Somers <bob@bobsomers.com>
2016-07-28 21:56:34 -0700

    Added rover12_msgs package and simulator package.

---
+++ src/simulator/src/simulator_node.cpp
@@ -0,0 +1,12 @@
+#include <ros/ros.h>
+
+int main(int argc, char* argv[]) {
+  ros::init(argc, argv, "simulator_node");
+  ros::NodeHandle node;
+
+  while(ros::ok()) {
+    // TODO
+  }
+
+  return 0;
+}
