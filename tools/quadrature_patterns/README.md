43cbe1d11d4a070f2c36f906a016ad1efaf96fa9
Bob Somers <bob@bobsomers.com>
2016-08-20 23:05:37 -0700

    Quadrature encoder pattern generator.

---
+++ tools/quadrature_patterns/README.md
@@ -0,0 +1,7 @@
+# Quadrature Patterns
+
+Install python3 and [Pillow](https://pillow.readthedocs.io) and then run
+`$ ./genpattern.py`.
+
+You can tweak the size, number of ticks, etc. of the generated quadrature
+pattern in the Python script.
---
+++ tools/quadrature_patterns/genpattern.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import math
+from PIL import Image, ImageDraw
+
+# Configure your quadrature pattern here.
+dpi = 300
+wheel_width_inches = 2
+wheel_diameter_inches = 4
+stripes_per_track = 30
+stripe_color = (0, 0, 0)
+
+# Calcuate the size of the image given our physical dimensions and our DPI.
+width_px = wheel_width_inches * dpi
+height_px = math.ceil(wheel_diameter_inches * math.pi * dpi)
+stripe_width_px = width_px / 2
+stripe_height_px = height_px / (2 * stripes_per_track)
+
+# Create a new image with the proper dimensions.
+im = Image.new("RGB", (width_px, height_px), (255, 255, 255))
+d = ImageDraw.Draw(im)
+
+# Draw first track.
+for i in range(0, stripes_per_track):
+    start = i * 2 * stripe_height_px
+    d.rectangle([(0, start), (stripe_width_px, start + stripe_height_px)], fill=stripe_color)
+
+# Draw second track.
+for i in range(0, stripes_per_track):
+    start = i * 2 * stripe_height_px + (stripe_height_px / 2)
+    d.rectangle([(stripe_width_px, start), (2 * stripe_width_px, start + stripe_height_px)], fill=stripe_color)
+
+# Save the image with the desired DPI.
+im.save("pattern.png", dpi=(dpi, dpi))
